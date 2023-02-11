790a8e8c6103cdfb4c746e3b34cdeb1470f0de47
fernandoralmeida <fernando.ralmeida@outlook.com>
2023-02-11 20:56:42 -0300

    2023-02-11 20:54

--- src/Data/SqlServer/CustomerService/Repositories/RepositoryCustomer.cs
+++ src/Data/SqlServer/CustomerService/Repositories/RepositoryCustomer.cs
@@ -9,38 +9,32 @@ namespace Sim.GRP.Data.SqlServer.CustomerService.Repositories;
 
 public class RepositoryCustomers : RepositoryBase<ECustomer>, IRepositoryCustomer, IDisposable
 {
-    public RepositoryCustomers(CustomerServiceContext context) : base(context) {}
+    public RepositoryCustomers(CustomerServiceContext context) : base(context) { }
 
-    public void Dispose()  => this.Dispose();
+    public void Dispose() => this.Dispose();
 
     public async Task<IEnumerable<ECustomer>> DoListAsync(Expression<Func<ECustomer, bool>>? param = null)
     {
-        if (_dbcontext.Customers!= null)
-        {
-            var _query = _dbcontext.Customers.AsQueryable();
 
-            if (param != null)
-                _query = _query
-                        .Include(i => i.Profile)
-                        .Include(i => i.Business)
-                        .Include(i => i.Fones)
-                        .Include(i => i.Emails)
-                        .Include(i => i.Locations)
-                        .Include(i => i.Attendances)
-                        .Where(param)
-                        .AsNoTrackingWithIdentityResolution();
+        var _query = _dbcontext.Customers!.AsQueryable();
 
-            return await _query.ToListAsync();
-        }
-        else
-            return new List<ECustomer>();
+        if (param != null)
+            _query = _query
+                    .Include(i => i.Profile)
+                    .Include(i => i.Business)
+                    .Include(i => i.Fones)
+                    .Include(i => i.Emails)
+                    .Include(i => i.Locations)
+                    .Include(i => i.Attendances)
+                    .Where(param)
+                    .AsNoTrackingWithIdentityResolution();
+
+        return await _query.ToListAsync();
+
     }
 
     public async Task<ECustomer> GetAsync(Guid id)
-    {
-        if (_dbcontext.Customers != null)
-        {
-            var qry = await _dbcontext.Customers
+        => await _dbcontext.Customers!
                             .Include(i => i.Profile)
                             .Include(i => i.Business)
                             .Include(i => i.Fones)
@@ -49,12 +43,8 @@ public class RepositoryCustomers : RepositoryBase<ECustomer>, IRepositoryCustome
                             .Include(i => i.Attendances)
                             .FirstOrDefaultAsync(s => s.Id == id);
 
-            if (qry != null)
-                return qry;
-            else
-                return new ECustomer();
-        }
-        else
-            return new ECustomer();
-    }
+            
+                
+
+    
 }
